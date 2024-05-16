b27bd92d0c043a61f7f577e3e1c15f44d9adcb2c
kamilly-quadros <kamiquadros20@gmail.com>
2024-05-16 13:33:21 -0400

    anotações

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importaar a classe Scanner
+        //TODO: Exibir as mensagens para o nosso usuário
+        //TODO: Obter pela classe scanner os valores digitados no terminal
+        //TODO: Exibir a mensagem da conta criada
     }
 }
