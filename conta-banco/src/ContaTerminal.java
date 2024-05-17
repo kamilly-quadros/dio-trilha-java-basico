2eaa42e4b586644f2c578979c06c59d54c6d8db6
kamilly-quadros <kamiquadros20@gmail.com>
2024-05-17 13:09:51 -0400

    Desafio Conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importaar a classe Scanner
-        //TODO: Exibir as mensagens para o nosso usuário
-        //TODO: Obter pela classe scanner os valores digitados no terminal
-        //TODO: Exibir a mensagem da conta criada
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int numero;
+        double saldo;
+        String agencia;
+        String nomeCliente;
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigada por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
